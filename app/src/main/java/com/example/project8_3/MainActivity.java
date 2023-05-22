4c6583676feac60c3faa23e0e1ae70b765d37229
kimsongie12 <127849839+kimsongie12@users.noreply.github.com>
2023-05-22 20:47:22 +0900

    project 8-3

---
+++ app/src/main/java/com/example/project8_3/MainActivity.java
@@ -0,0 +1,74 @@
+package com.example.project8_3;
+
+import java.io.File;
+
+import android.os.Environment;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    Button btnPrev, btnNext;
+    myPictureView myPicture;
+    TextView tvNumber;
+    int curNum=1;
+    File[] imageFiles;
+    String imageFname;
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        getSupportActionBar().setDisplayShowHomeEnabled(true);
+        getSupportActionBar().setIcon(R.drawable.ic_launcher);
+        setTitle("간단 이미지 뷰어 (변경)");
+        ActivityCompat.requestPermissions(this, new String[] {android.Manifest.permission.WRITE_EXTERNAL_STORAGE},MODE_PRIVATE);
+
+        btnPrev = (Button) findViewById(R.id.btnPrev);
+        btnNext = (Button) findViewById(R.id.btnNext);
+        tvNumber = (TextView) findViewById(R.id.tvNumber);
+        myPicture = (myPictureView) findViewById(R.id.myPictureView1);
+
+        imageFiles = new File(Environment.getExternalStorageDirectory()
+                .getAbsolutePath()+"/Pictures").listFiles();
+        imageFname = imageFiles[1].toString();
+        myPicture.imagePath = imageFname;
+        tvNumber.setText("1" + "/" + (imageFiles.length-1));
+
+        btnPrev.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                if (curNum <= 1) {
+                    curNum = imageFiles.length - 1;
+                } else {
+                    curNum--;
+                }
+                imageFname = imageFiles[curNum].toString();
+                myPicture.imagePath = imageFname;
+                myPicture.invalidate();
+                tvNumber.setText((curNum) + "/" + (imageFiles.length-1));
+
+            }
+        });
+
+        btnNext.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                if (curNum >= imageFiles.length - 1) {
+                    curNum = 1;
+                } else {
+                    curNum++;
+                }
+                imageFname = imageFiles[curNum].toString();
+                myPicture.imagePath = imageFname;
+                myPicture.invalidate();
+                tvNumber.setText((curNum) + "/" + (imageFiles.length-1));
+            }
+        });
+
+    }
+
+}
